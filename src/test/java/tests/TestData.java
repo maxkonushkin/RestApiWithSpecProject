b8cb2b11b7ff658a59e505d6948056cdb9b4fbe2
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-04-06 12:53:48 +0400

    add System.getProperty

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -2,8 +2,8 @@ package tests;
 
 public class TestData {
 
-    public static final String username = System.getProperty("username","qaguru");
-    public static final String password = System.getProperty("password","qaguru123");
+    public static final String username = System.getProperty("username");
+    public static final String password = System.getProperty("password");
     public static final String wrongPassword = "qaguru1234";
     public static final String wrongUsername = "qaguru1234";
 }
