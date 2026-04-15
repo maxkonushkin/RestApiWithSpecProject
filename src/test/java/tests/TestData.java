1fcd2f362c935c93ae7b0d96b9eefe2c4fc7b5bd
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-04-15 18:06:20 +0400

    add UI ReviewTests

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -2,8 +2,8 @@ package tests;
 
 public class TestData {
 
-    public static final String username = System.getProperty("username");
-    public static final String password = System.getProperty("password", "1234");
+    public static final String username = System.getProperty("username", "user8");
+    public static final String password = System.getProperty("password", "user8");
     public static final String wrongPassword = "qaguru1234";
     public static final String wrongUsername = "qaguru1234";
 }
